dd1d6b10e664b4556b7a75fb4d10926b077c067f
joehe <leanbrown@live.cn>
2018-03-20 16:27:37 +0800

    Initial Commit

---
+++ JHotelIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JHotelIOS
+//
+//  Created by 何苗 on 2018/3/20.
+//  Copyright © 2018年 joehe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JHotelIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JHotelIOS
+//
+//  Created by 何苗 on 2018/3/20.
+//  Copyright © 2018年 joehe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
